d793d76c7986477b6870d432df4847a33bcc3353
kubat <kubat.aiupov@gmail.com>
2023-03-15 21:54:19 +0100

    add styles

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const URL = "https://api.thecatapi.com/v1/images/search?limit=10";
+const URL = "https://api.thecatapi.com/v1/images/search?limit=9";
 let responseData = [];
 
 const btn = document.getElementById("generate-btn");
 const cardEl = document.querySelector(".card");
 
 const getRandomCat = async (url) => {
-  const response = await fetch(URL);
-  const data = await response.json();
-  responseData = data;
-  console.log(responseData);
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    responseData = data;
+    console.log(data);
+    catCard(data);
+  } catch (err) {
+    console.log("Error occured: ", err);
+  }
 };
 
+const catCard = (data) => {
+  const card = data
+    .map((cat) => `<img src=${cat.url} ${cat.width} ${cat.height}/>`)
+    .join("");
+  cardEl.innerHTML = card;
+};
 
-
-getRandomCat()
+btn.addEventListener("click", getRandomCat);
